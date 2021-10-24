349b61b53677822ca21af5658990607d4056ea65
CNine <cnine229@gmail.com>
2021-10-24 11:01:35 +0800

    chore: add server_test.go

--- server.go
+++ server.go
@@ -1,13 +1,31 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+	// Use an external setup function in order
+	// to configure the app in tests as well
+	app := Setup()
+
+	// start the application on http://localhost:3000
+	log.Fatal(app.Listen(":3000"))
+}
+
+func Setup() *fiber.App {
+	// Initialize a new app
 	app := fiber.New()
 
+	// Register the index route with a simple
+	// "OK" response. It should return status
+	// code 200
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("hello go fiber")
+		return c.SendString("OK")
 	})
 
-	app.Listen(":3000")
+	// Return the configured app
+	return app
 }
