5660a7acbb71934b2a20e6d78029a3ddbae46771
CNine <cnine229@gmail.com>
2021-12-04 22:24:21 +0800

    chore: ready for graphql

---
+++ internal/service/graph.go
@@ -0,0 +1,25 @@
+package service
+
+import (
+	"github.com/Aysnine/unripe-bison/internal/types"
+)
+
+// Graph godoc
+// @Summary graphql
+// @ID graphql
+// @Produce  json
+// @Router /api/graph [post]
+func SetupApi_Graph(setup *types.SetupContext) {
+	// app := setup.App
+	// db := setup.DB
+
+	// app.All("/query", func(c *fiber.Ctx) error {
+	// 	gqlHandler(c.Context())
+	// 	return nil
+	// })
+
+	// app.All("/playground", func(c *fiber.Ctx) error {
+	// 	playground(c.Context())
+	// 	return nil
+	// })
+}
